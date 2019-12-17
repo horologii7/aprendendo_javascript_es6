e804542962a843aa23aa0a787f5d9b48253a963d
horologii <ruanfelipedpontes@gmail.com>
2019-12-16 21:22:41 -0300

    Modulo webpack concluido

---
+++ webpack_server/src/main.js
@@ -0,0 +1,12 @@
+// 1a questao
+// 1.1
+//import ClasseUsuario from './functions';
+
+//ClasseUsuario.info();
+// 1.2
+//import {idade} from './functions';
+//alert(idade);
+// 1.3
+import ClasseUsuario, {idade as IdadeUsuario} from './functions';
+alert(IdadeUsuario);
+ClasseUsuario.info();
